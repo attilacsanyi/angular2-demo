8f2c46981f3f6c64a98dfd993552e149faf9fe09
Attila Csanyi <attila.csnyi@yahoo.co.uk>
2016-08-30 21:56:09 +0100

    refactor: remove empty ts, rename actions

--- src/app/counter/counter.action.ts
+++ src/app/counter/counter.action.ts
@@ -1,22 +1,22 @@
 import {BaseAction} from '../shared/models';
 
 // Counter Actions
-const PREFIX = 'COUNTER_';
+const PREFIX = '[Counter]';
 
 export class IncrementCounterAction extends BaseAction {
   constructor(public amount: number) {
-    super(`${PREFIX}INCREMENT`);
+    super(`${PREFIX} Increment`);
   }
 }
 
 export class DecrementCounterAction extends BaseAction {
   constructor(public amount: number) {
-    super(`${PREFIX}DECREMENT`);
+    super(`${PREFIX} Decrement`);
   }
 }
 
 export class ResetCounterAction extends BaseAction {
   constructor() {
-    super(`${PREFIX}RESET`);
+    super(`${PREFIX} Reset`);
   }
 }
