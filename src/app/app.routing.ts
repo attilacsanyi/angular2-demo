8b3a31e1f233307e007f2fe9811f20313108d97d
Attila Csanyi <attila.csnyi@yahoo.co.uk>
2016-10-13 12:09:51 +0100

    feat: enable preloading lazy routes (Angular 2.1.0 feature)

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -1,6 +1,11 @@
-import { RouterModule } from '@angular/router';
+import { RouterModule, PreloadAllModules } from '@angular/router';
 
-export const routing = RouterModule.forRoot([
-    { path: '', redirectTo: 'home', pathMatch: 'full' },
-    { path: 'counter', loadChildren: 'app/counter/counter.module' },
-]);
+export const routing = RouterModule.forRoot(
+    [
+        { path: '', redirectTo: 'home', pathMatch: 'full' },
+        { path: 'counter', loadChildren: 'app/counter/counter.module' },
+    ],
+
+    // Preload all Lazy modules while the user start navigating the app 
+    { preloadingStrategy: PreloadAllModules }
+);
